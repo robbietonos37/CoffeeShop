74cccc602d20d420094974d8ecc2544127ae150b
Robbie Tonos <Retonos@go.olemiss.edu>
2022-08-11 15:31:13 -0500

    Create Constructor, setters, and getters

---
+++ CoffeeShop.java
@@ -0,0 +1,44 @@
+public class CoffeeShop {
+
+    private int options;
+    private double prices;
+    private String coffee;
+    private int size;
+    public void coffeeShop(){
+        options = 0;
+        prices = 0.0;
+        coffee = "";
+        size = 0;
+    }
+
+    public void coffeeShop(int o, double p, String coffee, int s){
+        options = o;
+        prices = p;
+        this.coffee = coffee;
+        size = s;
+    }
+    public void setOptions(int o){
+        options = o;
+    }
+
+    public void setPrices(double p){
+        prices = p;
+    }
+
+    public void setCoffee(String coffee) {
+        this.coffee = coffee;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public int getOptions(){
+        return options;
+    }
+
+    public void printInfo(){
+        System.out.println();
+    }
+    
+}
---
+++ Driver.java
@@ -0,0 +1,9 @@
+public class Driver {
+
+    CoffeeShop coffeeShop = new CoffeeShop();
+
+    set.coffeeShop(5, 7, "Cold Brew", 9);
+
+
+    }
+}
