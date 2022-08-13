446aee01d39fc7b29122fe18c1c4bd24e9b2faeb
Robbie Tonos <Retonos@go.olemiss.edu>
2022-08-13 14:50:55 -0500

    Create Choice class to add choices to coffeeshop menu

---
+++ src/Choice.java
@@ -0,0 +1,16 @@
+public class Choice{
+    private String choiceName;
+
+    private double choicePrice;
+
+    public Choice(){
+    choiceName = "";
+    choicePrice = 0.0;
+    }
+
+    public Choice(String name, double price){
+    choiceName = name;
+    choicePrice = price;
+    }
+
+}
