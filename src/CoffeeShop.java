956402d6f5213df0221ed27dddf678014065a833
Robbie Tonos <Retonos@go.olemiss.edu>
2022-12-06 02:45:24 -0600

    Change textfield to top. Create setPrice method and new double "price"

--- src/CoffeeShop.java
+++ src/CoffeeShop.java
@@ -16,9 +16,8 @@ public class CoffeeShop implements ActionListener{
 
     JPanel panel;
 
-    JTextArea field2;
+    double price;
 
-    JLabel label;
 
     Font myFont = new Font("Ink Free", Font.BOLD, 21);
 
@@ -28,20 +27,22 @@ public class CoffeeShop implements ActionListener{
         //Create Frame(Biggest Window)
         JFrame frame = new JFrame("Welcome to " + name + "'s CoffeeShop");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
+        frame.setSize(500, 500);
+        frame.setLayout(null);
 
         //Create Text Field To Hold Price Totals
-        textField = new JTextField();
-        textField.setBounds(70, 25, 350, 50);
-        textField.setFont(myFont);
+          textField = new JTextField();
+          textField.setBounds(50, 25, 300, 50);
+          textField.setFont(myFont);
+
 
         //Disables the user from typing in the text box
         textField.setEditable(false);
 
         //Creates A Panel that will hold the buttons in the options
-        panel = new JPanel();
-        panel.setBounds(50, 100, 200, 200);
-        panel.setLayout(new GridLayout(4,4,10,10));
+          panel = new JPanel();
+          panel.setBounds(50, 100, 300, 300);
+          panel.setLayout(new GridLayout(4,4,10,10));
 
         //Creating buttons for our coffee options
         coldBrewButton = new JButton("Cold Brew");
@@ -62,22 +63,19 @@ public class CoffeeShop implements ActionListener{
           panel.add(icedCoffeeButton);
           panel.add(hotCoffeeButton);
 
-          field2 = new JTextArea();
 
-          field2.setBounds(450, 450, 300, 300);
-          field2.setFont(myFont);
-          field2.setText("Cart");
-          field2.setEditable(false);
-          field2.setLineWrap(false);
 
 
 
         frame.add(panel);
         frame.add(textField);
-        frame.add(field2, BorderLayout.EAST);
 
 
         frame.setVisible(true);
+    }
+
+    public void setPrice(double newPrice){
+          price += newPrice;
     }
 
     public static void main(String[] args) {
@@ -87,20 +85,21 @@ public class CoffeeShop implements ActionListener{
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() == coldBrewButton){
-            textField.setText("Your total will be $3.77");
-            field2.setText("\n" + field2.getText() + " Cold Brew");
+            this.setPrice(2);
+            textField.setText("Your total will be $" + price);
+            //field2.append("\nCold Brew");
         }
         else if(e.getSource() == espressoButton){
             textField.setText("Your total will be $2.13");
-            field2.setText("\n" + field2.getText() + " Espresso");
+            //field2.append("\nEspresso");
         }
         else if(e.getSource() == icedCoffeeButton){
             textField.setText("Your total will be $3.07");
-            field2.setText("\n" + field2.getText() + " Iced Coffee");
+            //field2.append("\nIced Coffee");
         }
         else if(e.getSource() == hotCoffeeButton){
             textField.setText("Your total will be $2.88");
-            field2.setText("\n" + field2.getText() + " Hot Coffee");
+            //field2.append("Hot Coffee");
         }
 
     }
